18e89614f52ee5b06d147de473ba8552a9851ade
sindyalvinaa <sindyalvinaa93@gmail.com>
2022-02-23 16:02:27 +0700

    Commit 1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,30 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\NewsController;
+use App\Http\Controllers\ProductsController;
+use App\Http\Controllers\ProgramController;
+use App\Http\Controllers\AboutUsController;
+use App\Http\Controllers\ContactUsController;
+
+Route::get ('/', [HomeController::class, 'index']);
+
+Route::prefix('category')->group(function (){
+    Route::get('/marbel-edu-games', [ProductsController::class, 'category1']);
+    Route::get('/marbel-and-friends-kids-games', [ProductsController::class, 'category2']);
+    Route::get('/riri-story-books', [ProductsController::class, 'category3']);
+    Route::get('/kolak-kids-songs', [ProductsController::class, 'category4']);
 });
+
+Route::get('/news/{id}', [NewsController::class, 'news']);
+
+Route::prefix('program')->group(function (){
+    Route::get('/karir', [ProgramController::class, 'program1']);
+    Route::get('/magang', [ProgramController::class, 'program2']);
+    Route::get('/kunjungan-industri', [ProgramController::class, 'program3']);
+});
+
+Route::get ('/', [AboutUsController::class, 'aboutus']);
+
+Route::resource('contact-us', ContactUsController::class);
